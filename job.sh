e8f71b49a927b3b1ecd5e95f5ccad3acc744c477
km1124 <km1124@ic.ac.uk>
2025-06-20 16:49:33 +0100

    job.sh

---
+++ job.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --partition=AMD7-A100-T
+#SBATCH --job-name=gpu-test
+#SBATCH --output=./logs/slurm-%j.log
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:1  # Request 1 GPU
+
+source /vol/cuda/12.0.0/setup.sh
+
+source .venv/bin/activate
+
+export UV_PYTHON_INSTALL_DIR="/vol/bitbucket/km1124/.cache/python"
+export HF_HOME="/vol/bitbucket/km1124/.cache/huggingface"
+export UV_CACHE_DIR="/vol/bitbucket/km1124/.cache/uv"
+
+export CONFIG="recipes/Llama-3.2-1B-Instruct/best_of_n.yaml"
+
+python scripts/test_time_compute.py $CONFIG
+
