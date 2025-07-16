b0ec75ed6c8d62606adebb8d0ce2987446317b69
kmilicic <kabo.milicic@gmail.com>
2025-07-16 18:38:29 +0100

    gitignore wandb

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -200,7 +200,7 @@ def checkout_commit(connection, config: DeployConfig) -> None:
         f"[yellow]Checking out commit with hash: {commit_hash}", spinner="dots"
     ):
         with connection.cd(config.remote_config.remote_root):
-            connection.run(f"git checkout {commit_hash}")
+            connection.run(f"git fetch && git checkout {commit_hash}")
     console.print(f"[green]Checked out commit with hash: {commit_hash}")
 
 
