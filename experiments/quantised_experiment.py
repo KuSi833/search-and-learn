c650c4a18bb8c812797a3d4885fd8b4312b97b24
km1124 <km1124@ic.ac.uk>
2025-07-30 17:35:23 +0100

    new mega updated version

--- experiments/quantised_experiment.py
+++ experiments/quantised_experiment.py
@@ -59,14 +59,5 @@ QC_CONFIG = Config(
 
 
 if __name__ == "__main__":
-    # config = replace(BEST_OF_N_CONFIG, generator_config=Q8_MODEL)
-    # config = replace(BEAM_SEARCH_CONFIG, generator_config=Q8_MODEL)
-    # config = replace(BEST_OF_N_CONFIG, generator_config=Q4_MODEL)
-    # config = replace(BEAM_SEARCH_CONFIG, generator_config=Q4_MODEL)
-    # config = replace(BEST_OF_N_CONFIG, generator_config=BASE_MODEL)
-    # config = replace(BEAM_SEARCH_CONFIG, generator_config=BASE_MODEL)
-    # config = replace(BEST_OF_N_CONFIG, generator_config=INSTRUCT_MODEL)
-    # config = replace(BEAM_SEARCH_CONFIG, generator_config=INSTRUCT_MODEL)
-
     config = QC_CONFIG
     run(config)
