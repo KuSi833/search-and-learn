4bb8b9cc12fcd89a4366e0fa29daaf7f1438cc9e
kmilicic <kabo.milicic@gmail.com>
2025-08-09 18:37:56 +0100

    flush

--- scripts/test.py
+++ scripts/test.py
@@ -1,7 +1,7 @@
 from vllm import LLM
 
-print("Starting to load model...")
-model_path = "/vol/bitbucket/km1124/models/Qwen2.5-Math-7B-Instruct"
+print("Starting to load model...", flush=True)
+model_path = "/vol/bitbucket/km1124/search-and-learn/models/Qwen2.5-Math-7B-Instruct/"
 # model_path = "/data/km1124/models/Qwen2.5-Math-7B-Instruct"
 llm = LLM(model=model_path)
 print("Model loaded successfully!")
