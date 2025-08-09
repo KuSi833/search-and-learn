518975eb3a22f44d030265a2e6bcb87090fa1fa9
kmilicic <kabo.milicic@gmail.com>
2025-08-09 19:54:08 +0100

    name

--- experiments/qwen_math.py
+++ experiments/qwen_math.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     INSTRUCT_MODEL = GeneratorConfig(
         # name="Qwen/Qwen2.5-Math-7B-Instruct",
         base_path=model_base_path,
-        name="Qwen2.5-Math-7B-Instruct",
+        name="Qwen/Qwen2.5-Math-7B-Instruct",
         parameter_count="7B",
     )
     BASE_MODEL = GeneratorConfig(
