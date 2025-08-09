fb41f9db59f07b96c459e4e5cb38c5b9309bbf0f
kmilicic <kabo.milicic@gmail.com>
2025-08-09 18:41:43 +0100

    1.5b

--- scripts/test.py
+++ scripts/test.py
@@ -1,7 +1,8 @@
 from vllm import LLM
 
 print("Starting to load model...", flush=True)
-model_path = "/vol/bitbucket/km1124/search-and-learn/models/Qwen2.5-Math-7B-Instruct/"
+# model_path = "/vol/bitbucket/km1124/search-and-learn/models/Qwen2.5-Math-7B-Instruct/"
+model_path = "/vol/bitbucket/km1124/search-and-learn/models/Qwen2.5-Math-1.5B-Instruct/"
 # model_path = "/data/km1124/models/Qwen2.5-Math-7B-Instruct"
 llm = LLM(model=model_path)
 print("Model loaded successfully!")
