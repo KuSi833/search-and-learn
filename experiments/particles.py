6af00169fe4c86f3d444749398c6d82e6ec06446
kmilicic <kabo.milicic@gmail.com>
2025-08-12 14:49:28 +0100

    mistakes run

--- experiments/particles.py
+++ experiments/particles.py
@@ -34,12 +34,12 @@ if __name__ == "__main__":
 
     WANDB_CONFIG = WandbConfig(tags=set(["particles", "diagnostic"]))
     # DATASET_CONFIG = DatasetConfig(num_samples=100)
-    # DATASET_CONFIG = DatasetConfig(num_samples=500)
-    DATASET_CONFIG = DatasetConfig(
-        num_samples=100,
-        dataset_start=40,
-        dataset_end=45,
-    )
+    DATASET_CONFIG = DatasetConfig(num_samples=500)
+    # DATASET_CONFIG = DatasetConfig(
+    #     num_samples=100,
+    #     dataset_start=40,
+    #     dataset_end=45,
+    # )
 
     BASE_CONFIG = BaseConfig(
         prm_config=PRM_CONFIG,
