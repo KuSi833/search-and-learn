afb75c5611fa1ef86ca4dc554c459a4243544e64
kmilicic <kabo.milicic@gmail.com>
2025-08-21 14:58:53 +0100

    coverage = 10

--- experiments/qwen_math.py
+++ experiments/qwen_math.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
 
     DATASET_CONFIG = DatasetConfig.from_subset_file(
         run_id="53vig20u",
-        coverage=20,
+        coverage=10,
         benchmark=Benchmarks.MATH500.value,
         project_root=project_root,
     )
